a9f4fdde599889503f63a29a6dc3f57cfea16a7a
jenconn <ye1wyy@gmail.com>
2025-04-18 17:27:08 +0700

    update: update config, now uses package cleanenv

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,39 @@
 package config
 
-type Config interface {
-	MustLoad()
+import (
+	"auth-service/internal/database"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env                     string `env:"env" env-default:"local"`
+	HttpServer              `env:"server"`
+	database.PostgresConfig `env:"postgres"`
 }
 
-type config struct {
+type HttpServer struct {
+	Address string `env:"address" env-default:"localhost"`
+	Port    string `env:"port" env-default:"8080"`
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is empty")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("There is no file in the CONFIG_PATH path: %v", err)
+	}
+
+	var cfg Config
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Cannot read config file: %v", err)
+	}
+
+	return &cfg
 }
